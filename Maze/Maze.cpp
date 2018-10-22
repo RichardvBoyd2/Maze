f19ef14a63217f0cff6d8a373561a4fcb52c9317
Richard Boyd <34223701+RichardvBoyd2@users.noreply.github.com>
2018-10-21 17:01:53 -0700

    Ver 1.6
    Ver 1.6 modifies the program to convert the maze into a graph of linked nodes, and uses a Depth First Search to reach the end. Output is the same as Ver 1.3

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -9,7 +9,6 @@
 #include <algorithm>
 #include <vector>
 #include <stack>
-#include <queue>
 
 using namespace std;
 
@@ -20,7 +19,6 @@ char wall[3][3] = { {'X','X','X'},{'X','X','X'},{'X','X','X'} };
 char path[3][3] = { {',',',',','},{',',',',','},{',',',',','} };
 int boxX = 3, boxY = 3;
 int mazeX, mazeY;
-int finishX, finishY;
 string mazeboxes[20][20]; //supports mazes up to 20 by 20 squares big
 
 //opens or creates output file to write maze in
@@ -28,7 +26,7 @@ ofstream outFile("c:\\users\\rick\\source\\repos\\Maze\\Maze\\output_file.txt");
 
 
 void printMaze() {
-	
+
 	//loops through coordinates, checking for the type of square and writing the correct characters to write the maze to the output file.
 	int x = 0;
 	for (int y = 0; y < mazeY; y++) {
@@ -74,31 +72,20 @@ void printMaze() {
 	outFile << " " << endl;
 }
 
-struct qBox {
+class node {
+
 public:
-	int x, y, manDist;
-
-	qBox() {};
-	qBox(int i, int j) {
-		x = i;
-		y = j;
-		manDist = abs(finishX - x) + abs(finishY - y);
+	int x;
+	int y;
+	node() {}
+	node(int xx, int yy) {
+		x = xx;
+		y = yy;
 	}
-
-	int getX() {
-		return x;
-	}
-
-	int getY() {
-		return y;
-	}
-	
-};
-
-struct Compare {
-	bool operator()(const qBox &lhs, const qBox &rhs) const {
-		return lhs.manDist < rhs.manDist;
-	}
+	node* north = NULL;
+	node* east = NULL;
+	node* south = NULL;
+	node* west = NULL;
 };
 
 int main() {
@@ -155,9 +142,7 @@ int main() {
 		startpos.push_back(stoi(section));
 	}
 	mazeboxes[startpos[0]][startpos[1]] = "start";
-	int ratX = startpos[0];
-	int ratY = startpos[1];
-	
+
 	//splits the finishing location position section and set coordinates for finishing location
 	stringstream finishstream(splitinput[2]);
 	vector<int> finishpos;
@@ -165,9 +150,6 @@ int main() {
 		finishpos.push_back(stoi(section));
 	}
 	mazeboxes[finishpos[0]][finishpos[1]] = "finish";
-	finishX = finishpos[0];
-	finishY = finishpos[1];
-	qBox start = qBox(startpos[0], startpos[1]);
 
 	//splits the wall coordinates and sets all positions of wall squares
 	stringstream wallstream(splitinput[3]);
@@ -181,128 +163,72 @@ int main() {
 		wallcount = wallcount + 2;
 	}
 
-	printMaze();
 
-	//puzzle solving loop using stack
-	/*stack <string> route; 
-	while (mazeboxes[ratX][ratY] != "finish") { 
-		mazeboxes[ratX][ratY] = "path";
-
-		if (mazeboxes[ratX + 1][ratY] == "open" | mazeboxes[ratX + 1][ratY] == "finish") {
-			ratX++;
-			route.push("west");
-			cout << route.top() << endl;
-			continue;
-		}
-		else if (mazeboxes[ratX][ratY + 1] == "open" | mazeboxes[ratX][ratY + 1] == "finish") {
-			ratY++;
-			route.push("south");
-			cout << route.top() << endl;
-			continue;
-		}
-		else if (mazeboxes[ratX - 1][ratY] == "open" | mazeboxes[ratX - 1][ratY] == "finish") {
-			ratX--;
-			route.push("east");
-			cout << route.top() << endl;
-			continue;
-		}
-		else if (mazeboxes[ratX][ratY - 1] == "open" | mazeboxes[ratX][ratY - 1] == "finish") {
-			ratY--;
-			route.push("north");
-			cout << route.top() << endl;
-			
-			continue;
-		}
-		else {
-			string last = route.top();
-			route.pop();
-			if (last == "west") {
-				ratX--;
-			}
-			else if (last == "south") {
-				ratY--;
-			}
-			else if (last == "east") {
-				ratX++;
-			}
-			else if (last == "north") {
-				ratY++;
-			}
-		}
-		
-	}*/
-
-
-	//puzzle solving loop using queue
-	/*queue<qBox> paths;
-	paths.push(start);
-	int testX, testY;
-	testX = paths.front().getX();
-	testY = paths.front().getY();
-	while (mazeboxes[testX][testY] != "finish") {
+	printMaze(); //prints maze before solving it
 	
-		mazeboxes[testX][testY] = "path";
-
-		if (mazeboxes[testX + 1][testY] == "open" | mazeboxes[testX + 1][testY] == "finish") {
-			paths.push(qBox(testX + 1, testY));
+	//runs through maze and makes nodes for all open spaces
+	vector<node> graph;
+	for (int i = 0; i < mazeY; i++) {
+		for (int j = 0; j < mazeX; j++) {
+			if (mazeboxes[j][i] == "start") {
+				node start(j, i);
+				graph.push_back(start);
+			}
+			if (mazeboxes[j][i] == "open" | mazeboxes[j][i] == "finish") {
+				graph.emplace_back(node(j, i));
+			}
 		}
-		if (mazeboxes[testX][testY + 1] == "open" | mazeboxes[testX][testY + 1] == "finish") {
-			paths.push(qBox(testX, testY + 1));
-		}
-		if (mazeboxes[testX - 1][testY] == "open" | mazeboxes[testX - 1][testY] == "finish") {
-			paths.push(qBox(testX - 1, testY));
-		}
-		if (mazeboxes[testX][testY - 1] == "open" | mazeboxes[testX][testY - 1] == "finish") {
-			paths.push(qBox(testX, testY - 1));
-		}
-				
-		paths.pop();
-		testX = paths.front().getX();
-		testY = paths.front().getY();
-	}*/
-
-	priority_queue<qBox, vector<qBox>, Compare> paths;
-	paths.push(start);
-	int testX, testY;
-	testX = paths.top().x;
-	testY = paths.top().y;
-	while (mazeboxes[testX][testY] != "finish") {
-
-		mazeboxes[testX][testY] = "path";
-		/*cout << "size "; cout << paths.size() << endl;
-		cout << "top x "; cout << paths.top().x << endl;
-		cout << "top y "; cout << paths.top().y << endl;
-		system("pause");*/
-		paths.pop();
-		//printMaze();
-		
-
-		if (mazeboxes[testX + 1][testY] == "open" | mazeboxes[testX + 1][testY] == "finish") {
-			paths.push(qBox(testX + 1, testY));
-			//cout << "right" << endl;
-		}
-		if (mazeboxes[testX][testY + 1] == "open" | mazeboxes[testX][testY + 1] == "finish") {
-			paths.push(qBox(testX, testY + 1));
-			//cout << "down" << endl;
-		}
-		if (mazeboxes[testX - 1][testY] == "open" | mazeboxes[testX - 1][testY] == "finish") {
-			paths.push(qBox(testX - 1, testY));
-			//cout << "left" << endl;
-		}
-		if (mazeboxes[testX][testY - 1] == "open" | mazeboxes[testX][testY - 1] == "finish") {
-			paths.push(qBox(testX, testY - 1));
-			//cout << "up" << endl;
-		}
-		
-		testX = paths.top().x;
-		testY = paths.top().y;
-
 	}
 
+	//loops through graph and links any nodes that are next to each other
+	for (int i = 0; i < graph.size(); i++) {
+		for (int j = 0; j < graph.size(); j++) {
+			if (graph[i].x - graph[j].x == -1 && graph[i].y == graph[j].y) {
+				graph[i].west = &graph[j];
+			}
+			if (graph[i].x - graph[j].x == 1 && graph[i].y == graph[j].y) {
+				graph[i].east = &graph[j];
+			}
+			if (graph[i].y - graph[j].y == -1 && graph[i].x == graph[j].x) {
+				graph[i].south = &graph[j];
+			}
+			if (graph[i].y - graph[j].y == 1 && graph[i].x == graph[j].x) {
+				graph[i].north = &graph[j];
 
+			}
+		}
+	}
+
+	//Depth First Search
+	stack <node> route;
+	route.push(graph.front());
+	int ratX = route.top().x;
+	int ratY = route.top().y;
+	while (mazeboxes[ratX][ratY] != "finish") {
+		mazeboxes[ratX][ratY] = "path";
+
+		if (route.top().west != NULL && mazeboxes[(*route.top().west).x][(*route.top().west).y] != "path") {
+			route.push(*route.top().west);
+		}
+		else if (route.top().south != NULL && mazeboxes[(*route.top().south).x][(*route.top().south).y] != "path") {
+			route.push(*route.top().south);
+		}
+		else if (route.top().east != NULL && mazeboxes[(*route.top().east).x][(*route.top().east).y] != "path") {
+			route.push(*route.top().east);
+		}
+		else if (route.top().north != NULL && mazeboxes[(*route.top().north).x][(*route.top().north).y] != "path") {
+			route.push(*route.top().north);
+		}
+		else {
+			route.pop();
+		}
+
+		ratX = route.top().x;
+		ratY = route.top().y;
+	}
 
 	printMaze();
-
+	
 	//displays message to console once finished and pauses
 	cout << "Check output_file.txt to see the maze!" << endl;
 	system("pause");
